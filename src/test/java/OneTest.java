829f666c40a0d0d1aeffce023a7a1974acee862f
EldarAbdulhakov <braid2012@gmail.com>
2025-10-25 16:48:23 +0300

    1 commit

--- src/test/java/OneTest.java
+++ src/test/java/OneTest.java
@@ -18,7 +18,7 @@ public class OneTest {
         RemoteWebDriver driver;
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
