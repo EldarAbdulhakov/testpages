75b4e2be32b1f4a67846f40731d3c7c6e2cf4e07
EldarAbdulhakov <braid2012@gmail.com>
2025-10-25 03:29:20 +0300

    1 commit

--- src/test/java/OneTest.java
+++ src/test/java/OneTest.java
@@ -2,14 +2,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class OneTest {
 
     @Test
-    public void testSauceDemo() {
-        WebDriver driver = new ChromeDriver();
+    public void testSauceDemo() throws MalformedURLException {
+        WebDriver driver;
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        String selenoidUrl = System.getenv("SELENOID_URL");
+
+        if (selenoidUrl == null || selenoidUrl.isEmpty()) {
+            throw new IllegalStateException("SELENOID_URL is not set!");
+        }
+
+        driver = new RemoteWebDriver(new URL(selenoidUrl), options);
 
         driver.get("https://www.saucedemo.com");
 
